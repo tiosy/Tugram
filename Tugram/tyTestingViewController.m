3f4473020aa3ea8972fa76c9f97e2098c841bfca
Timothy Yeh <timothy.yeh@gmail.com>
2015-04-08 13:47:48 -0700

    test func

--- Tugram/tyTestingViewController.m
+++ Tugram/tyTestingViewController.m
@@ -9,6 +9,8 @@
 #import "tyTestingViewController.h"
 #import <Parse/Parse.h>
 
+#import "TUPFUser.h"
+
 #import "TUUser.h"
 #import "TUPhoto.h"
 #import "TUComment.h"
@@ -33,100 +35,144 @@
 //    testObject[@"foo22"] = @"bar23";
 //    [testObject saveInBackground];
 
-//*****************************************************************
-////  ADD to Parse: create User
-//*****************************************************************
-//
-//    TUUser *user = [TUUser object];
-//    user.username = @"jackson";
-//    user.fullName = @"michael Jackson";
-//    user.email = @"mjackson@google.com";
-//
-//    UIImage *image = [UIImage imageNamed:@"person"];
-//    NSData *imageData = UIImagePNGRepresentation(image);
-//
-//    PFFile *imagePFFile = [PFFile fileWithName:@"person.png" data:imageData];
-//
-//    user.profileThumbnailPFFile = imagePFFile;
-//
-//   [user saveInBackground];
 
+
+
+
+//  TESTING SUBCLASS PFUSER ...ADD to Parse: create User
+    [self testAddPFUser];
+
+//  ADD to Parse: create User
+    [self testAddTUUser];
 
 // update object/image to Parse
-
-//    PFQuery *query = [TUUser query];
-//        [query whereKey:@"username" equalTo:@"jackson"];
-//        [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
-//            if (!error) {
-//                // The find succeeded.
-//    
-//                TUUser *user = [objects firstObject];
-//
-//                //now updating
-//                user.email = @"mjackson@google.com";
-//                UIImage *image = [UIImage imageNamed:@"person"];
-//                NSData *imageData = UIImagePNGRepresentation(image);
-//                PFFile *imageFile = [PFFile fileWithName:@"person.png" data:imageData];
-//                
-//                user.profileThumbnailImage = imageFile;
-//                
-//                
-//                [user saveInBackground];
-//
-//
-//
-//            } else {
-//                // Log details of the failure
-//                NSLog(@"Error: %@ %@", error, [error userInfo]);
-//            }
-//        }];
-//
+[self testUpdateTUUserObjectImage];
 
 
-
-//***************************************************
-//  retrieve object & image from Parse
-//***************************************************
-
-    PFQuery *query = [TUUser query];
-    [query whereKey:@"username" equalTo:@"jackson"];
-    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
-        if (!error) {
-            // The find succeeded.
-
-            TUUser *user = [objects firstObject];
-            self.labelFullName.text = user.fullName;
-            self.labelEmail.text = user.email;
-
-
-            //retrieving image from Parse
-
-            PFFile *userPFile = user.profileThumbnailPFFile;
-            [userPFile getDataInBackgroundWithBlock:^(NSData *imageNSData, NSError *error) {
-                 NSLog(@"=from parse error=====%@",error);
-
-                if (!error) {
-                   UIImage *img = [UIImage imageWithData:imageNSData];
-
-                    NSLog(@"=from parse img=====%@",img);
-                   self.imageview.image = img;
-                }
-
-
-            }];
-
-            } else {
-            // Log details of the failure
-            NSLog(@"Error: %@ %@", error, [error userInfo]);
-        }
-    }];
-
-
-
-
+// Retrieve object/image from Parse
+[self testRetrieveTUUser];
 
 
 }
+
+
+
+////  TESTING SUBCLASS PFUSER ...ADD to Parse: create User
+-(void) testAddPFUser{
+
+            TUPFUser *user = [TUPFUser object];
+            user.username = @"onedirection";
+            user.password =@"thisIsMyPassword";
+            user.fullName = @"One Direction";
+            user.email = @"1direction@google.com";
+    
+            UIImage *image = [UIImage imageNamed:@"person"];
+            NSData *imageData = UIImagePNGRepresentation(image);
+            user.profileThumbnailImage = imageData;
+    
+            [user signUpInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+    
+            }];
+
+
+}
+
+
+//  ADD to Parse: create TUUser
+-(void) testAddTUUser{
+    
+        TUUser *user = [TUUser object];
+        user.username = @"jackson";
+        user.fullName = @"michael Jackson";
+        user.email = @"mjackson@google.com";
+    
+        UIImage *image = [UIImage imageNamed:@"person"];
+        NSData *imageData = UIImagePNGRepresentation(image);
+    
+        PFFile *imagePFFile = [PFFile fileWithName:@"person.png" data:imageData];
+    
+        user.profileThumbnailPFFile = imagePFFile;
+    
+       [user saveInBackground];
+
+}
+
+// update object/image to Parse
+-(void) testUpdateTUUserObjectImage
+{
+        PFQuery *query = [TUUser query];
+            [query whereKey:@"username" equalTo:@"jackson"];
+            [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+                if (!error) {
+                    // The find succeeded.
+    
+                    TUUser *user = [objects firstObject];
+    
+                    //now updating
+                    user.email = @"mjackson@google.com";
+                    UIImage *image = [UIImage imageNamed:@"person"];
+                    NSData *imageData = UIImagePNGRepresentation(image);
+                    PFFile *imageFile = [PFFile fileWithName:@"person.png" data:imageData];
+    
+                    user.profileThumbnailPFFile = imageFile;
+    
+    
+                    [user saveInBackground];
+    
+    
+    
+                } else {
+                    // Log details of the failure
+                    NSLog(@"Error: %@ %@", error, [error userInfo]);
+                }
+            }];
+}
+
+//  retrieve object & image from Parse
+-(void) testRetrieveTUUser{
+
+        PFQuery *query = [TUUser query];
+        [query whereKey:@"username" equalTo:@"jackson"];
+        [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+            if (!error) {
+                // The find succeeded.
+    
+                TUUser *user = [objects firstObject];
+                self.labelFullName.text = user.fullName;
+                self.labelEmail.text = user.email;
+    
+    
+                //retrieving image from Parse
+    
+                PFFile *userPFile = user.profileThumbnailPFFile;
+                [userPFile getDataInBackgroundWithBlock:^(NSData *imageNSData, NSError *error) {
+                     NSLog(@"=from parse error=====%@",error);
+    
+                    if (!error) {
+                       UIImage *img = [UIImage imageWithData:imageNSData];
+    
+                        NSLog(@"=from parse img=====%@",img);
+                       self.imageview.image = img;
+                    }
+    
+    
+                }];
+    
+                } else {
+                // Log details of the failure
+                NSLog(@"Error: %@ %@", error, [error userInfo]);
+            }
+        }];
+    
+    
+    
+    
+    
+    
+
+}
+
+
 
 
 
